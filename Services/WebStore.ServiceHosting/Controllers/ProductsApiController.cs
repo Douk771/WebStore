775c81f79b0aef5a5f0a5e61c74531ffff16b313
IK_Kolomeets <IK_Kolomeets@altke.ru>
2020-03-02 15:39:48 +0700

    Клиент для WebAPI для IProductData

--- Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
 using WebStore.Domain.DTO.Products;
 using WebStore.Domain.Entities;
 using WebStore.Interfaces.Services;
@@ -11,7 +12,7 @@ using WebStore.Interfaces.Services;
 namespace WebStore.ServiceHosting.Controllers
 {
     //[Route("api/[controller]")]
-    [Route("api/products")]
+    [Route(WebAPI.Products)]
     [ApiController]
     public class ProductsApiController : ControllerBase, IProductData
     {
